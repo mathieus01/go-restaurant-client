1528235558051f64188a4afa66dc52ad2442b51d
Matheus Nunes <mathieusnunes@gmail.com>
2021-03-04 08:36:40 -0300

    test: add AxiosHttpClient in infra layer

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,4 +1,12 @@
 import { HttpClient, HttpRequest, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
+import faker from 'faker'
+
+export const mockHttpRequest = (): HttpRequest => ({
+  url: faker.internet.url(),
+  method: faker.random.arrayElement(['get', 'post', 'put', 'delete']),
+  body: faker.random.objectElement(),
+  headers: faker.random.objectElement()
+})
 
 export class HttpClientSpy<R> implements HttpClient<R> {
   url?: string
