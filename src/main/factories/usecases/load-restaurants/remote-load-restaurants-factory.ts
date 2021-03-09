72a8e612281affc1a2c702faa30e4f09d778144b
Matheus Nunes <mathieusnunes@gmail.com>
2021-03-09 08:46:04 -0300

    feat: add RestaurantList page to main layer

--- src/main/factories/usecases/load-restaurants/remote-load-restaurants-factory.ts
+++ src/main/factories/usecases/load-restaurants/remote-load-restaurants-factory.ts
@@ -1,7 +1,8 @@
 import { makeAuthorizeHttpClientDecorator } from '@/main/factories/decorators'
 import { RemoteLoadRestaurants } from '@/data/usecases'
 import { LoadRestaurants } from '@/domain/usecases'
+import { makeApiUrl } from '../../http'
 
 export const makeRemoteLoadRestaurants = (): LoadRestaurants => {
-  return new RemoteLoadRestaurants('/restaurants', makeAuthorizeHttpClientDecorator())
+  return new RemoteLoadRestaurants(makeApiUrl('/restaurants'), makeAuthorizeHttpClientDecorator())
 }
--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-Router-dom'
-import { makeSignUp, makeLogin } from '@/main/factories/pages'
+import { makeSignUp, makeLogin, makeRestaurantList } from '@/main/factories/pages'
+import { setCurrentAccountAdapter, getCurrentAccountAdapter } from '@/main/adapters'
 import { ApiContext } from '@/presentation/contexts'
-import { setCurrentAccountAdapter } from '../adapters'
-import { RestaurantList } from '@/presentation/pages'
+import { PrivateRoute } from '@/presentation/components'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const Router: React.FC = () => {
   return (
     <ApiContext.Provider
       value={{
-        setCurrentAccount: setCurrentAccountAdapter
+        setCurrentAccount: setCurrentAccountAdapter,
+        getCurrentAccount: getCurrentAccountAdapter
       }}>
       <BrowserRouter>
         <Switch>
           <Route path="/signup" exact component={makeSignUp} />
           <Route path="/login" exact component={makeLogin} />
-          <Route path="/" component={RestaurantList} />
+          <PrivateRoute path="/" component={makeRestaurantList} />
         </Switch>
       </BrowserRouter>
     </ApiContext.Provider>
