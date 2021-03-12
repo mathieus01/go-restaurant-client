77ee1e0463e72bd268bcbae4632c96a31b10cefa
Matheus Nunes <mathieusnunes@gmail.com>
2021-03-12 06:17:33 -0300

    feat: add FoodList to router in main layer

--- src/main/factories/pages/index.ts
+++ src/main/factories/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './signup/signup-factory'
 export * from './login/login-factory'
 export * from './restaurant-list/restaurant-list-factory'
+export * from './food-list/food-list-factory'
--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { makeSignUp, makeLogin, makeRestaurantList } from '@/main/factories/pages'
+import { makeSignUp, makeLogin, makeRestaurantList, makeFoodList } from '@/main/factories/pages'
 import { setCurrentAccountAdapter, getCurrentAccountAdapter } from '@/main/adapters'
 import { ApiContext } from '@/presentation/contexts'
 import { PrivateRoute } from '@/presentation/components'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { FoodList } from '@/presentation/pages'
 
 const Router: React.FC = () => {
   return (
@@ -18,7 +17,7 @@ const Router: React.FC = () => {
           <Route path="/signup" exact component={makeSignUp} />
           <Route path="/login" exact component={makeLogin} />
           <PrivateRoute path="/" exact component={makeRestaurantList} />
-          <PrivateRoute path="/restaurants/:id" component={FoodList} />
+          <PrivateRoute path="/restaurants/:id" component={makeFoodList} />
         </Switch>
       </BrowserRouter>
     </ApiContext.Provider>
