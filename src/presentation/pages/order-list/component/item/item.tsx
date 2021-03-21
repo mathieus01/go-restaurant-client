bec32d0eb44b12e994d4cc815c5a6da6b17a92c2
Matheus Nunes <mathieusnunes@gmail.com>
2021-03-21 16:17:49 -0300

    fix: fix order date in order-item

--- src/presentation/pages/order-list/component/item/item.tsx
+++ src/presentation/pages/order-list/component/item/item.tsx
@@ -1,6 +1,7 @@
 import { LoadOrders } from '@/domain/usecases'
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import moment from 'moment'
 import Styles from './item-styles.scss'
 
 type Props = {
@@ -19,14 +20,14 @@ const Item: React.FC<Props> = ({ order }: Props) => {
     <li className={Styles.orderItemWrap} onClick={goToDetail}>
       <div className={Styles.orderDateWrapper}>
         <div className={Styles.orderDate}>
-          <span className={Styles.day}>{order.date.getDate().toString().padStart(2, '0')}</span>
-          <span className={Styles.month}>{order.date.toLocaleString('pt-BR', { month: 'short' }).replace('.', '')}</span>
+          <span className={Styles.day}>{moment(order.date).format('DD')}</span>
+          <span className={Styles.month}>{moment(order.date).format('MMM')}</span>
         </div>
       </div>
       <div className={Styles.orderDetail}>
         <h2>{restaurant.name}</h2>
         {order.foodsOrder.map(foodOrder => (
-          <span key={foodOrder.id}>{foodOrder.amount}x {foodOrder.food.name}</span>
+          <span key={foodOrder.id}>{foodOrder.amount}x {foodOrder.food.name} </span>
         ))}
         <h4>Status: {order.status}</h4>
       </div>
