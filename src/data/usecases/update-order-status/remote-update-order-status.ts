ae0eb1447d31611fef41ade90437920c222b6417
Matheus Nunes <mathieusnunes@gmail.com>
2021-03-28 10:58:15 -0300

    feat: add RemoteUpdateOrderStatus usecase in data layer

---
+++ src/data/usecases/update-order-status/remote-update-order-status.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpStatusCode } from '@/data/protocols/http'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
+import { UpdateOrderStatus } from '@/domain/usecases'
+
+export class RemoteUpdateOrderStatus implements UpdateOrderStatus {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpClient
+  ) {}
+
+  async updateOrderStatus (params: UpdateOrderStatus.Params): Promise<void> {
+    const { orderId, status } = params
+    const httpResponse = await this.httpClient.request({
+      url: `${this.url}/${orderId}`,
+      body: { status },
+      method: 'put'
+    })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unauthorized: throw new AccessDeniedError()
+      case HttpStatusCode.noContent: return null
+      default: throw new UnexpectedError()
+    }
+  }
+}
